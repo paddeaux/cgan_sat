381bd7028db46b7db30a9076c7029fca630d9d7d
paddeaux <gorrypaddy@gmail.com>
2023-08-25 12:20:43 +0100

    Fixed creation

--- create_seasons.py
+++ create_seasons.py
@@ -3,17 +3,18 @@ import pandas as pd
 import pathlib
 from tqdm import tqdm
 
-path = "/data/pgorry/sen12ms/s2/"
+path = "/data/pgorry/sen12ms/s2"
 filenames = list(pathlib.Path(path).glob("*/*/*.tif"))
-foldernames = list(pathlib.Path(path).glob("*/*/"))
+foldernames = list(pathlib.Path(path).glob("*/*"))
 seasons_original = pd.read_csv('/data/pgorry/sen12ms/seasons.csv')
-
+print(seasons_original)
 seasons = []
 filenames = []
 
 for folder in foldernames:
     for file in list(pathlib.Path(folder).glob("*.tif")):
         season_ref = "_".join(os.path.basename(file).split("_")[0:4])
+        season_ref = season_ref.replace("_s2_", "_")
         season = seasons_original.loc[seasons_original["scene"] == season_ref]["true_season"]
         filenames.append(os.path.basename(file))
         seasons.append(season.item())
@@ -26,6 +27,6 @@ winter = [1 if x == 'winter' else -1 for x in seasons]
 df = pd.DataFrame(zip(filenames, spring, summer, fall, winter), columns=['scene','spring', 'summer', 'fall', 'winter'])
 
 print("Saving data...")
-df.to_csv("/data/pgorry/sen12ms/s2/seasons_labeled.csv", index=False)
+df.to_csv("/data/pgorry/sen12ms/seasons_labeled.csv", index=False)
 print("Saved.")
 print(df.head())
