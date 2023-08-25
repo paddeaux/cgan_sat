59f2fa093f15231bfc8254a875ed494ffc427459
paddeaux <gorrypaddy@gmail.com>
2023-08-25 11:30:48 +0100

    Corrected filepaths

--- create_seasons.py
+++ create_seasons.py
@@ -3,15 +3,12 @@ import pandas as pd
 import pathlib
 from tqdm import tqdm
 
-path = "C:/Users/Paddy/CRT/Github/input/SEN12MS/"
+path = "/data/pgorry/input/sen12ms/s2"
 filenames = list(pathlib.Path(path).glob("*/*/*.tif"))
-
-path = "C:/Users/Paddy/CRT/Github/input/SEN12MS/ROIs1158_spring"
-# s1 = fall, s6 = spring
 
 foldernames = list(pathlib.Path(path).glob("*/"))
 
-seasons_original = pd.read_csv('C:/Users/Paddy/CRT/Github/input/SEN12MS/seasons_spring.csv')
+seasons_original = pd.read_csv('/data/pgorry/sen12ms/seasons.csv')
 
 seasons = []
 filenames = []
@@ -31,6 +28,6 @@ winter = [1 if x == 'winter' else -1 for x in seasons]
 df = pd.DataFrame(zip(filenames, spring, summer, fall, winter), columns=['scene','spring', 'summer', 'fall', 'winter'])
 
 print("Saving data...")
-df.to_csv("seasons_labeled_spring.csv", index=False)
+df.to_csv("/data/pgorry/sen12ms/s2/seasons_labeled.csv", index=False)
 print("Saved.")
 print(df.head())
